88a65a26a2e99f659b3e2ad591e8e2fb0b05c985
Your Name <youremail@example.com>
2025-12-15 16:36:49 +0500

    trying to fix error: apis are not working in production

--- middleware.ts
+++ middleware.ts
@@ -43,6 +43,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * - public folder
          */
-        "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+        "/((?!api|_next/static|_next/image|favicon.ico|public/).*)",
       ],
 }
